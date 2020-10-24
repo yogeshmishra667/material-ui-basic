40337609b459b8014a19d8aa75f79aa3b2795c56
YoGi <yogeshmishra667@gmail.com>
2020-10-24 17:29:17 +0530

    added customization(theme provider)

--- src/app.js
+++ src/app.js
@@ -3,10 +3,32 @@ import Button from '@material-ui/core/Button';
 import SaveIcon from '@material-ui/icons/Save';
 import DeleteIcon from '@material-ui/icons/Delete';
 import { ButtonGroup } from '@material-ui/core';
+import Checkbox from './components/checkbox';
+import TextField from '@material-ui/core/TextField';
+import CustomStyle from './components/custom-style';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { orange, green } from '@material-ui/core/colors';
+
+//create and modify theme
+const theme = createMuiTheme({
+  palette: {
+    secondary: {
+      main: orange[500],
+    },
+    primary: {
+      main: green[500],
+    },
+  },
+});
 
 export default function App() {
   return (
-    <div>
+    //for theme
+    <ThemeProvider theme={theme}>
+      {/* if yo want to add two primary color you can also use two theme provider */}
+      <TextField variant="outlined" label="name" />
+      <Checkbox></Checkbox>
+      <CustomStyle />
       <ButtonGroup variant="contained">
         <Button startIcon={<SaveIcon />} color="primary">
           save
@@ -15,6 +37,6 @@ export default function App() {
           delete
         </Button>
       </ButtonGroup>
-    </div>
+    </ThemeProvider>
   );
 }
