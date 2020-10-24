71e5116c842a1e08f9f7b129a305ef1a5d5f6906
YoGi <yogeshmishra667@gmail.com>
2020-10-24 16:00:16 +0530

    added check box

---
+++ src/components/checkbox.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+import Checkbox from '@material-ui/core/Checkbox';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import BluetoothAudioIcon from '@material-ui/icons/BluetoothAudio';
+import BluetoothDisabledIcon from '@material-ui/icons/BluetoothDisabled';
+
+export default function Check() {
+  const [checked, setChecked] = useState(true);
+  return (
+    <div>
+      <FormControlLabel label="yogesh mishra">
+        <Checkbox
+          size="small"
+          checked={checked}
+          icon={<BluetoothDisabledIcon />}
+          checkedIcon={<BluetoothAudioIcon />}
+          onChange={(e) => setChecked(e.target.checked)}
+          color="primary"
+        ></Checkbox>
+      </FormControlLabel>
+    </div>
+  );
+}
