440b4165086d2dc158c9ea1f5ab758171c138fd6
YoGi <yogeshmishra667@gmail.com>
2020-10-26 17:20:51 +0530

    grid responsive

--- src/components/grid.js
+++ src/components/grid.js
@@ -4,15 +4,15 @@ import Grid from '@material-ui/core/Grid';
 
 export default function grid() {
   return (
-    <Grid container spacing={5}>
-      <Grid item>
-        <Paper style={{ width: '50px', height: '60px' }} elevation={3}></Paper>
+    <Grid container spacing={5} justify="center">
+      <Grid item xs={3} sm={12}>
+        <Paper style={{ width: '100', height: '60px' }} elevation={3}></Paper>
       </Grid>
-      <Grid item>
-        <Paper style={{ width: '50px', height: '60px' }} elevation={3}></Paper>
+      <Grid item xs={3} sm={6}>
+        <Paper style={{ width: '45', height: '60px' }} elevation={3}></Paper>
       </Grid>
-      <Grid item>
-        <Paper style={{ width: '50px', height: '60px' }} elevation={3}></Paper>
+      <Grid item xs={3} sm={6}>
+        <Paper style={{ width: '45', height: '60px' }} elevation={3}></Paper>
       </Grid>
     </Grid>
   );
